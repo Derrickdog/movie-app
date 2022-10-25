076d8276d65f2a93bc857abec25b7e6f81ac8a46
Derrick <derrickryan10@gmail.com>
2022-10-25 12:21:47 -0400

    Fix https bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import MovieCard from './MovieCard';
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=aeabd256'
+const API_URL = 'https://www.omdbapi.com/?apikey=aeabd256'
 
 const App = () => {
   const [movies, setMovies] = useState([]);
